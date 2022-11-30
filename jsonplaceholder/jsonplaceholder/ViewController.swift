36e62aa934ff626b21d1410034babe4046d0cb65
matiaz <dmatiaz@icloud.com>
2022-11-30 08:56:54 -0600

    master: created the xcode project
    master: created the xcode project

---
+++ jsonplaceholder/jsonplaceholder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  jsonplaceholder
+//
+//  Created by dmatiaz on 29/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
